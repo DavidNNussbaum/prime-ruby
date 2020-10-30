9c33695bcc8175ef55b9899079162abee073635e
DavidNNussbaum <brocknynj@aol.com>
2020-10-30 17:06:30 -0400

    commit tree 227e71fe3d8938ccc98f3733c39c4c9bee71f572

--- prime.rb
+++ prime.rb
@@ -1,1 +1,67 @@
-# Add  code here!
+require 'pry'
+def prime?(number)
+    if number <= 0
+        return false
+    elsif number == 1
+        return false
+    else  
+       for divisor in 2..(number - 1)
+           if (number % divisor) == 0
+             return false
+          end
+      end
+        return true
+    end
+end
+# binding.pry
+
+
+
+
+
+
+
+# def prime?(number)
+#     divisor = (2.. number-1).to_a
+#     array.each do |divisor|
+#     # for divisor.each in 2..(number - 1)
+#         if (number % divisor) == 0
+#           false
+#         end
+    
+        
+#           true
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# is_prime = true
+# def prime?(n)
+#     if n == 2
+#       return is_prime = true
+  
+#     else for i in 2..n-1
+         
+#          if n % i == 0
+#             return is_prime = false
+#     end
+#   end
+# end
+
+# end
+
+ 
+ 
